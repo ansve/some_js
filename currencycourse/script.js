55d5d0d1759a767b5e34c8773454bb7ac724149b
Andrei <ansve@ua.fm>
2020-09-30 19:10:56 +0300

    currency table js

--- currencycourse/script.js
+++ currencycourse/script.js
@@ -1,21 +1,57 @@
-function refreshCurrensyCourse () {
+function refreshCurrensyCourse (currensy) {
     const req = new XMLHttpRequest();
     req.addEventListener('load', function() {
+        const table = document.querySelector('tbody');
+        table.innerHTML = null;
         const data = JSON.parse(this.responseText);
-        const currensyInfo = document.querySelector('#currencyList');
-
-        console.log(data);
+        data.forEach(p => {
+                //console.log(`Валюта: ${p.cc} Имя валюты: ${p.txt} Курс: ${p.rate} Дата торгов: ${p.exchangedate}`);
+                let str = p.cc;
+                 if (str.startsWith(currensy.toUpperCase())){
+                    
+                    table.innerHTML += `<tr><td>${p.cc}</td><td>${p.txt}</td><td>${p.rate}</td><td>${p.exchangedate}</td></tr>`;
+                 }
+                
+                //console.log(str);
+            });
+        
     });
-    req.open('GET', 'https://bank.gov.ua/NBUStatService/v1/statdirectory/exchangenew?valcode=GBP&json', true);
+    req.open('GET', 'https://bank.gov.ua/NBUStatService/v1/statdirectory/exchangenew?json', true);
     req.send();
 }
 
-refreshCurrensyCourse();
 
 
-// var xhr = new XMLHttpRequest();
-// xhr.open('GET', 'https://bank.gov.ua/NBUStatService/v1/statdirectory/exchangenew?json', true);
-// xhr.onload = function () {
-//   console.log(xhr.responseURL); // http://example.com/test
-// };
-// xhr.send(null);
+
+
+const input = document.querySelector('#currencyName');
+    input.addEventListener('input', event=> {
+        let currensy = input.value;
+        refreshCurrensyCourse(currensy);
+            //console.log(currensy);
+        
+        
+        
+    });
+
+
+
+
+/*
+$('button').click(function(){
+    $.get('https://bank.gov.ua/NBUStatService/v1/statdirectory/exchangenew?json').success(
+                function(data) {
+             data.forEach(p => {
+                console.log(`Валюта: ${p.cc} Имя валюты: ${p.txt} Курс: ${p.rate} Дата торгов: ${p.exchangedate}`);
+            });
+            alert('Success!');
+        });
+});
+
+
+
+$('table').append('<tr><td>Валюта</td><td>Валюта</td><td>???</td><td>???</td></tr>');
+*/
+
+// var obj = {"r030":156,"txt":"Юань Женьміньбі","rate":3.9727,"cc":"CNY","exchangedate":"10.08.2020"};
+// console.log(Object.keys(obj));
